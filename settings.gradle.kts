75a0d7a0ecbe3543712d7b66192d6d520baf7209
JeongJeongYong <wjddyd94@naver.com>
2025-03-19 22:45:14 +0900

    build: update root gradle files for connecting extra modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,4 @@
-rootProject.name = "backend-sample-multi-module"
+rootProject.name = "${extra["ROOT_PROJECT_NAME"]}"
 
+apply(from ="core/settings.gradle.kts")
+apply(from ="common/settings.gradle.kts")
