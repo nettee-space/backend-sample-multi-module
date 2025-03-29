794d89686bd4abceadd3aee538e0af6e99cd6400
merge-simpson <letsdev@kakao.com>
2025-03-29 15:32:04 +0900

    build(board): scan board-related modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,8 @@
 rootProject.name = "backend-sample-multi-module"
 
+val services = "${rootProject.projectDir}/services"
+
 apply(from = "common/common.settings.gradle.kts")
 apply(from = "core/core.settings.gradle.kts")
+
+apply(from = "$services/board/board.settings.gradle.kts")
