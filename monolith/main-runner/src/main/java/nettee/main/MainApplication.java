d3a204c29c09d614f634e4f3174b2e642cfa6d26
silberbullet <pkw961028@naver.com>
2025-03-30 18:02:30 +0900

    build(monolith): add board yml

--- monolith/main-runner/src/main/java/nettee/main/MainApplication.java
+++ monolith/main-runner/src/main/java/nettee/main/MainApplication.java
@@ -3,7 +3,7 @@ package nettee.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "nettee")
 public class MainApplication {
     
     public static void main(String[] args) {
