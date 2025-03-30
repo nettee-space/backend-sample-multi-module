5518ff9da21ba264649eadb6182482833e7d94c3
JeongJeongYong <wjddyd94@naver.com>
2025-03-30 12:46:26 +0900

    chore(board): add `mapstruct` dependencies to `rdb` build.gradle.kts

--- services/board/driven/rdb/build.gradle.kts
+++ services/board/driven/rdb/build.gradle.kts
@@ -10,4 +10,9 @@ dependencies {
     // querydsl
     implementation("com.querydsl:querydsl-jpa:${bom["querydsl.version"]}:jakarta")
     annotationProcessor("com.querydsl:querydsl-apt:${bom["querydsl.version"]}:jakarta")
+
+    // mapstruct
+    implementation("org.mapstruct:mapstruct:1.6.3")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
+    annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
 }
