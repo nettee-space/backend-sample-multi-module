8ea192b1b62878e16aece4664462c855e3cf0039
silberbullet <pkw961028@naver.com>
2025-03-30 17:55:54 +0900

    feat(monolith): add dependencies board and core

--- monolith/main-runner/build.gradle.kts
+++ monolith/main-runner/build.gradle.kts
@@ -3,7 +3,14 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 version = "0.0.1-SNAPSHOT"
 
 dependencies {
+    // core
+    implementation(project(":exception-handler-core"))
+    implementation(project(":jpa-core"))
+    // service
+    implementation(project(":board"))
+    // webmvc
     implementation("org.springframework.boot:spring-boot-starter-web")
+    // test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
