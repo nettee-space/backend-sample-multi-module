02ebc11f184fb2137047265550c30f1e0c72d5cd
silberbullet <pkw961028@naver.com>
2025-04-01 02:10:56 +0900

    fix(core): delete domain

--- core/jpa-core/src/main/java/nettee/jpa/config/JpaConfig.java
+++ core/jpa-core/src/main/java/nettee/jpa/config/JpaConfig.java
@@ -7,8 +7,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @EnableJpaAuditing
-@EntityScan(basePackages = "me.nettee") // 엔티티 패키지 지정
-@EnableJpaRepositories(basePackages = "me.nettee") // 리포지토리 패키지 지정
+@EntityScan(basePackages = "nettee") // 엔티티 패키지 지정
+@EnableJpaRepositories(basePackages = "nettee") // 리포지토리 패키지 지정
 public class JpaConfig {
 
 }
