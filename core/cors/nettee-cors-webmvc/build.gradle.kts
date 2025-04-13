40625d673552b86f2fb0b4ded5fa796c9200044b
silberbullet <pkw961028@naver.com>
2025-04-13 21:15:54 +0900

    build(cors): add cors api in webmvc module

--- core/cors/nettee-cors-webmvc/build.gradle.kts
+++ core/cors/nettee-cors-webmvc/build.gradle.kts
@@ -1,3 +1,6 @@
 dependencies {
+    api(project(":cors-api"))
     compileOnly("org.springframework:spring-webmvc:6.2.2")
+    // test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
