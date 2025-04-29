ec69f40b11c653609704cd2935c9bf2353e23529
merge-simpson <letsdev@kakao.com>
2025-04-30 00:24:18 +0900

    refactor(common): add constants about status code

--- common/src/main/java/nettee/common/status/StatusCodeUtil.java
+++ common/src/main/java/nettee/common/status/StatusCodeUtil.java
@@ -2,13 +2,17 @@ package nettee.common.status;
 
 import nettee.common.marker.TypeSafeMarker.Present;
 
+import static nettee.common.status.StatusCodeConstants.Default.CATEGORY_SHIFT;
+import static nettee.common.status.StatusCodeConstants.Default.GENERAL_PURPOSE_SHIFT;
+import static nettee.common.status.StatusCodeConstants.Default.SYSTEM_INFORMATION_SHIFT;
+
 public final class StatusCodeUtil {
     private StatusCodeUtil() {}
 
     public static int getAsInt(StatusParameters<Present, Present> parameters) {
-        return (parameters.generalPurposeBits() << 24)
-                | (parameters.systemInfoBits() << 16)
-                | (parameters.categoryBits() << 8)
+        return (parameters.generalPurposeBits() << GENERAL_PURPOSE_SHIFT)
+                | (parameters.systemInfoBits() << SYSTEM_INFORMATION_SHIFT)
+                | (parameters.categoryBits() << CATEGORY_SHIFT)
                 | parameters.instanceBits();
     }
 }
