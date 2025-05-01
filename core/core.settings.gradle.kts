d6b2663851a7076ca35caceab07152e6dde57662
merge-simpson <letsdev@kakao.com>
2025-05-02 00:35:00 +0900

    build(time): add time-util module

--- core/core.settings.gradle.kts
+++ core/core.settings.gradle.kts
@@ -6,12 +6,14 @@ val core = rootDir.resolve("core")
 
 
 include(
+    ":time-util",
     ":jpa-core",
     ":exception-handler-core",
     ":cors-api",
     ":cors-webmvc"
 )
 
+project(":time-util").projectDir = core["time-util"]!!
 project(":jpa-core").projectDir = core["jpa-core"]!!
 project(":exception-handler-core").projectDir = core["exception-handler-core"]!!
 project(":cors-webmvc").projectDir = core["nettee-cors-webmvc"]!!
