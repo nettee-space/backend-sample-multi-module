aaa88cab768a9ef38cb02631fc36802cf3bd8e15
silberbullet <pkw961028@naver.com>
2025-05-01 18:35:02 +0900

    fix(jpa-core): delete AuditingEntityListener

--- core/jpa-core/src/main/java/nettee/jpa/support/SnowflakeBaseEntity.java
+++ core/jpa-core/src/main/java/nettee/jpa/support/SnowflakeBaseEntity.java
@@ -1,17 +1,14 @@
 package nettee.jpa.support;
 
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import nettee.hibenate.annotation.SnowflakeGenerated;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.io.Serializable;
 
 @Getter
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
 public abstract class SnowflakeBaseEntity implements Serializable {
     @Id
     @SnowflakeGenerated
