d5a66ee096f272ab2b329c0975c0d7cb6e0e318d
silberbullet <pkw961028@naver.com>
2025-05-03 17:45:34 +0900

    fix(core): delete file

--- core/rest-client/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-dependencies {
-    compileOnly("org.springframework:spring-web:6.2.0")
-    compileOnly("org.springframework.boot:spring-boot-autoconfigure:3.4.3")
-
-    // webmvc
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    // webmvc
-    testImplementation("org.springframework.boot:spring-boot-starter-web")
-}
