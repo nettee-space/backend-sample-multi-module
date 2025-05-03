83f8084860df136feb7de80cc3274fddd2850a56
sweetykr7 <sweetykr@naver.com>
2025-05-03 14:48:53 +0900

    fix(post): fix gradle setting

--- services/post/post.settings.gradle.kts
+++ services/post/post.settings.gradle.kts
@@ -1,8 +1,17 @@
-var post = rootDir.resolve("services/post")
-        .walkTopDown()
-        .maxDepth(3)
-        .filter(File::isDirectory)
-        .associateBy(File::getName)
+fun getDirectories(vararg names: String): (String) -> File {
+    var dir = rootDir
+    for (name in names) {
+        dir = dir.resolve(name)
+    }
+    return { targetName ->
+        val directory = dir.walkTopDown().maxDepth(3)
+                .filter(File::isDirectory)
+                .associateBy { it.name }
+        directory[targetName] ?: throw Error("그런 폴더가 없습니다: $targetName")
+    }
+}
+
+val post = getDirectories("services", "post")
 
 include (
         ":post",
@@ -15,12 +24,11 @@ include (
         ":post:webmvc-adapter",
 )
 
-project(":post").projectDir = post["post"]!!
-project(":post:api").projectDir = post["api"]!!
-project(":post:api:domain").projectDir = post["domain"]!!
-project(":post:api:exception").projectDir = post["exception"]!!
-project(":post:api:readmodel").projectDir = post["readmodel"]!!
-project(":post:application").projectDir = post["application"]!!
-project(":post:rdb-adapter").projectDir = post["rdb"]!!
-project(":post:webmvc-adapter").projectDir = post["web-mvc"]!!
-
+project(":post").projectDir = post("post")
+project(":post:api").projectDir = post("api")
+project(":post:api:domain").projectDir = post("domain")
+project(":post:api:exception").projectDir = post("exception")
+project(":post:api:readmodel").projectDir = post("readmodel")
+project(":post:application").projectDir = post("application")
+project(":post:rdb-adapter").projectDir = post("rdb")
+project(":post:webmvc-adapter").projectDir = post("web-mvc")
