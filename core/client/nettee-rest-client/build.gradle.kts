f3111bea684a9d469aa02b7328eca08e36209ae9
silberbullet <pkw961028@naver.com>
2025-05-04 20:25:30 +0900

    build(rest-client): add test library

--- core/client/nettee-rest-client/build.gradle.kts
+++ core/client/nettee-rest-client/build.gradle.kts
@@ -2,4 +2,9 @@ dependencies {
     api(project(":client-api"))
     // Spring Release on 2025.4.17
     compileOnly("org.springframework:spring-web:6.2.6")
+    // test
+    testImplementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("com.h2database:h2")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-data-jpa")
 }
