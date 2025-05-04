bc1ef22c0202da8e09a0fef0f1b8245e639ba16d
JeongJeongYong <wjddyd94@naver.com>
2025-05-04 14:34:18 +0900

    fix(common): modularize `EnumUtil` in common module

--- common/enum-util/src/main/java/nettee/common/EnumUtil.java
+++ common/enum-util/src/main/java/nettee/common/EnumUtil.java
@@ -8,19 +8,10 @@ public class EnumUtil {
 
     private EnumUtil() {}
 
-    /**
-     * 모든 열거형(enum)의 필드 값이 고유한지 검사합니다.
-     * @param enumClass
-     * @param getter
-     * @return
-     * @param <E>
-     * @param <T>
-     */
     public static <E extends Enum<E>, T> boolean isUniqueAllOf(Class<E> enumClass, Function<E, T> getter) {
         return EnumSet.allOf(enumClass).stream()
                 .map(getter)
                 .collect(Collectors.toSet())
                 .size() == enumClass.getEnumConstants().length;
     }
-
 }
