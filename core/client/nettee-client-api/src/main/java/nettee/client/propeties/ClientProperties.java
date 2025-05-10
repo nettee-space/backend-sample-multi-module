c698a1a5c89a50bcd560a907db303c20c747279c
silberbullet <pkw961028@naver.com>
2025-05-10 22:38:56 +0900

    feat(client-api): add logging library

--- core/client/nettee-client-api/src/main/java/nettee/client/propeties/ClientProperties.java
+++ core/client/nettee-client-api/src/main/java/nettee/client/propeties/ClientProperties.java
@@ -1,19 +1,22 @@
-package netttee.client.propeties;
+package nettee.client.propeties;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.Map;
+import java.util.Map.Entry;
 
+@Slf4j
 @ConfigurationProperties("app.client")
 public record ClientProperties(
         String baseUrl,
         Map<String, String> url
 ) {
     public ClientProperties {
-        System.out.println("default url: " + baseUrl);
+        log.debug("baseUrl url: {}", baseUrl);
         
-        for(Map.Entry<String, String> entry : url.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
+        for(Entry<String, String> entry : url.entrySet()) {
+            log.debug((entry.getKey() + ": " + entry.getValue()));
         }
     }
 }
