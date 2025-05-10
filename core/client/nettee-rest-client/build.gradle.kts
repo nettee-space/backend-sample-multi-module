928841d87dec19c8d9a48bcd1a71a9143db13dd8
silberbullet <pkw961028@naver.com>
2025-05-10 22:42:03 +0900

    build(rest-client): add object mapper library

--- core/client/nettee-rest-client/build.gradle.kts
+++ core/client/nettee-rest-client/build.gradle.kts
@@ -1,7 +1,7 @@
 dependencies {
     api(project(":client-api"))
-    // Spring Release on 2025.4.17
-    compileOnly("org.springframework:spring-web:6.2.6")
+    api("org.springframework:spring-web:6.2.3")
+    compileOnly("com.fasterxml.jackson.core:jackson-databind")
     // test
     testImplementation("org.springframework.boot:spring-boot-starter-web")
     testImplementation("com.h2database:h2")
