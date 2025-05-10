18363b1dc56f8864f3d506c6de1ca859563f8b38
silberbullet <pkw961028@naver.com>
2025-05-10 22:39:39 +0900

    refactor(client-api): delete custom exception and add body adn unwrapKey

--- core/client/nettee-client-api/src/main/java/nettee/client/request/NetteeRequest.java
+++ core/client/nettee-client-api/src/main/java/nettee/client/request/NetteeRequest.java
@@ -1,18 +1,17 @@
-package netttee.client.request;
+package nettee.client.request;
 
 import lombok.Builder;
-import nettee.common.CustomException;
 
 import java.util.Objects;
-import java.util.function.Supplier;
 
 /**
  * HTTP 공통 요청 레코드
  *
  * @param domain          요청할 도메인 키 (예: "board", "auth")
- * @param path            요청할 경로 (예: "comments/{id}")
+ * @param path            요청할 경로 (예: "/comments/{id}")
+ * @param body            요청할 본문 객체로 POST, PATCH, PUT 등의 사용 (null 허용)
  * @param responseType    응답을 매핑할 타입 클래스
- * @param customException 상태코드가 에러일 경우 던질 사용자 정의 예외 (null 허용)
+ * @param unwrapKey       Wrap 형식의 JSON 역직렬화 키 (null 허용)
  * @param uriVariables    URI 경로 변수 (예: {id}에 들어갈 값들)
  * @param <T>             응답 타입 제네릭
  */
@@ -20,8 +19,9 @@ import java.util.function.Supplier;
 public record NetteeRequest<T>(
         String domain,
         String path,
+        Object body,
         Class<T> responseType,
-        Supplier<? extends CustomException> customException,
+        String unwrapKey,
         Object... uriVariables
 ) {
     public NetteeRequest {
@@ -30,5 +30,9 @@ public record NetteeRequest<T>(
         
         domain = domain.strip();
         path = path.strip();
+        
+        if(unwrapKey == null) {
+            unwrapKey = domain;
+        }
     }
 }
