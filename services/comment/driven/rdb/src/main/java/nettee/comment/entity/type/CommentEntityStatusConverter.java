de2beaac50fab641ea9ee3553cf1839f079f4244
seonghooni <lsh3717@naver.com>
2025-05-11 22:14:55 +0900

    feat(comment/reply): add EntityStatusConverter

---
+++ services/comment/driven/rdb/src/main/java/nettee/comment/entity/type/CommentEntityStatusConverter.java
@@ -0,0 +1,18 @@
+package nettee.comment.entity.type;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter
+public class CommentEntityStatusConverter implements AttributeConverter<CommentEntityStatus, Integer> {
+
+    @Override
+    public Integer convertToDatabaseColumn(CommentEntityStatus status) {
+        return status.getCode();
+    }
+
+    @Override
+    public CommentEntityStatus convertToEntityAttribute(Integer value) {
+        return CommentEntityStatus.valueOf(value);
+    }
+}
