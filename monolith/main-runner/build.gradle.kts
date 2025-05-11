746daea78fb24f30204bd174f49b9b7ea12be9f3
seonghooni <lsh3717@naver.com>
2025-05-11 22:19:26 +0900

    fix(comment/reply): resolve conflict monolith/main-runner/build.gradle.kts

--- monolith/main-runner/build.gradle.kts
+++ monolith/main-runner/build.gradle.kts
@@ -1,5 +1,8 @@
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
+val board: String by project
+val comment: String by project
+
 version = "0.0.1-SNAPSHOT"
 
 dependencies {
@@ -7,16 +10,22 @@ dependencies {
     implementation(project(":exception-handler-core"))
     implementation(project(":jpa-core"))
     implementation(project(":cors-webmvc"))
-    // service
-    implementation(project(":board"))
-    implementation(project(":comment"))
+
+    // services
+    implementation(project(board))
+    implementation(project(comment))
+
     // webmvc
     implementation("org.springframework.boot:spring-boot-starter-web")
+
     // db
     runtimeOnly("org.postgresql:postgresql:42.7.4")
+
     // flyway
     implementation("org.flywaydb:flyway-database-postgresql")
+
     // test
+    testImplementation("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("com.fasterxml.jackson.core:jackson-databind")
     testImplementation("com.fasterxml.jackson.module:jackson-module-kotlin")
