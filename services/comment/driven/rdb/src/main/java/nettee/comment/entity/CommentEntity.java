970cfc441b4478c753c09874554687a0e289037c
seonghooni <lsh3717@naver.com>
2025-05-18 18:55:53 +0900

    refactor(comment/reply): add builder method to entity

--- services/comment/driven/rdb/src/main/java/nettee/comment/entity/CommentEntity.java
+++ services/comment/driven/rdb/src/main/java/nettee/comment/entity/CommentEntity.java
@@ -1,7 +1,9 @@
 package nettee.comment.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
+import java.util.Objects;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,14 +19,40 @@ import org.hibernate.annotations.DynamicUpdate;
 @Entity(name = "comment")
 public class CommentEntity extends LongBaseTimeEntity {
 
+    @Column(nullable = false)
+    public Long boardId;
+
     public String content;
 
     @Convert(converter = CommentEntityStatusConverter.class)
     public CommentEntityStatus status;
 
     @Builder
-    public CommentEntity(String content, CommentEntityStatus status) {
+    public CommentEntity(Long boardId, String content, CommentEntityStatus status) {
+        this.boardId = boardId;
         this.content = content;
+        this.status = status;
+    }
+
+    @Builder(
+        builderClassName = "updateCommentEntityBuilder",
+        builderMethodName = "prepareCommentEntityUpdate",
+        buildMethodName = "update"
+    )
+    public void update(String content) {
+        Objects.requireNonNull(content, "Content cannot be null!");
+
+        this.content = content;
+    }
+
+    @Builder(
+        builderClassName = "updateStatusCommentEntityBuilder",
+        builderMethodName = "prepareCommentEntityStatusUpdate",
+        buildMethodName = "updateStatus"
+    )
+    public void updateStatus(CommentEntityStatus status) {
+        Objects.requireNonNull(status, "status cannot be null");
+
         this.status = status;
     }
 }
--- services/comment/driven/rdb/src/main/java/nettee/reply/entity/ReplyEntity.java
+++ services/comment/driven/rdb/src/main/java/nettee/reply/entity/ReplyEntity.java
@@ -1,7 +1,9 @@
 package nettee.reply.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
+import java.util.Objects;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,14 +19,40 @@ import org.hibernate.annotations.DynamicUpdate;
 @Entity(name = "reply")
 public class ReplyEntity extends LongBaseTimeEntity {
 
+    @Column(nullable = false)
+    public Long commentId;
+
     public String content;
 
     @Convert(converter = ReplyEntityStatusConverter.class)
     public ReplyEntityStatus status;
 
     @Builder
-    public ReplyEntity(String content, ReplyEntityStatus status) {
+    public ReplyEntity(Long commentId, String content, ReplyEntityStatus status) {
+        this.commentId = commentId;
         this.content = content;
+        this.status = status;
+    }
+
+    @Builder(
+        builderClassName = "updateReplyEntityBuilder",
+        builderMethodName = "prepareReplyEntityUpdate",
+        buildMethodName = "update"
+    )
+    public void update(String content) {
+        Objects.requireNonNull(content, "Content cannot be null!");
+
+        this.content = content;
+    }
+
+    @Builder(
+        builderClassName = "updateStatusReplyEntityBuilder",
+        builderMethodName = "prepareReplyEntityStatusUpdate",
+        buildMethodName = "updateStatus"
+    )
+    public void updateStatus(ReplyEntityStatus status) {
+        Objects.requireNonNull(status, "status cannot be null");
+
         this.status = status;
     }
 }
