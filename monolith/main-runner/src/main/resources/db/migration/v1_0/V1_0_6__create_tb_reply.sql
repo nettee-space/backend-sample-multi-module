2283fee0eef0bf6205445cf43df17ce6d1f095ef
merge-simpson <letsdev@kakao.com>
2025-05-23 22:24:40 +0900

    refactor(main-runner): move flyway DDLs to correct directory

--- monolith/main-runner/src/main/resources/db/migration/v1_0/V1_0_6__create_tb_reply.sql
+++ monolith/main-runner/src/main/resources/db/migration/v1_0/V1_0_6__create_tb_reply.sql
@@ -15,8 +15,8 @@ CREATE TABLE IF NOT EXISTS article.reply (
 COMMENT ON TABLE article.reply IS '답글';
 
 -- 컬럼 코멘트
-COMMENT ON COLUMN article.reply.comment_id   IS '부모 댓글 ID';
-COMMENT ON COLUMN article.reply.content   IS '내용';
+COMMENT ON COLUMN article.reply.comment_id  IS '부모 댓글 ID';
+COMMENT ON COLUMN article.reply.content     IS '내용';
 COMMENT ON COLUMN article.reply.status      IS '상태';
 COMMENT ON COLUMN article.reply.created_at  IS '생성시간';
 COMMENT ON COLUMN article.reply.updated_at  IS '마지막 수정시간';
