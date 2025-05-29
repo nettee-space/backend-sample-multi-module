7116304f46e72d4ade2ee30a640967739c5a11db
sweetykr7 <sweetykr@naver.com>
2025-05-29 14:29:36 +0900

    feature(draft): add null check

--- services/article/driven/rdb/src/main/java/nettee/draft/driven/rdb/entity/type/DraftEntityStatusConverter.java
+++ services/article/driven/rdb/src/main/java/nettee/draft/driven/rdb/entity/type/DraftEntityStatusConverter.java
@@ -2,11 +2,18 @@ package nettee.draft.driven.rdb.entity.type;
 
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
+import nettee.draft.exception.DraftQueryErrorCode;
+import nettee.draft.exception.DraftQueryException;
 
 @Converter
 public class DraftEntityStatusConverter implements AttributeConverter<DraftEntityStatus, Integer> {
     @Override
-    public Integer convertToDatabaseColumn(DraftEntityStatus status) { return status.getCode(); }
+    public Integer convertToDatabaseColumn(DraftEntityStatus status) {
+        if (status == null) {
+            throw new DraftQueryException(DraftQueryErrorCode.DEFAULT);
+        }
+        return status.getCode();
+    }
 
     @Override
     public DraftEntityStatus convertToEntityAttribute(Integer value) { return DraftEntityStatus.valueOf(value); }
