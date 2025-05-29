43abc69031aa13d191a4573ea183b4c5319295d9
silberbullet <pkw961028@naver.com>
2025-05-30 01:25:04 +0900

    feat(redis): add redis standalone connect factory

---
+++ core/redis/nettee-redis-standalone/src/main/java/nettee/redis/config/RedisStandaloneConfig.java
@@ -0,0 +1,18 @@
+package nettee.redis.config;
+
+import nettee.redis.properties.RedisProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+@EnableConfigurationProperties(RedisProperties.class)
+public class RedisStandaloneConfig {
+    
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(RedisProperties redisProperties) {
+        return new LettuceConnectionFactory(redisProperties.host(), redisProperties.port());
+    }
+}
