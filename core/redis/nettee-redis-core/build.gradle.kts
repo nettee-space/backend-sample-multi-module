f4198302a660bac609f6c1098b3f6999001876e0
silberbullet <pkw961028@naver.com>
2025-05-30 11:00:44 +0900

    build(redis): rename redis-standalone to redis core

--- core/redis/nettee-redis-core/build.gradle.kts
+++ core/redis/nettee-redis-core/build.gradle.kts
@@ -1,6 +1,5 @@
 dependencies{
     api(project(":redis-api"))
-
-    compileOnly("org.springframework.boot:spring-context")
+    compileOnly("org.springframework:spring-context")
     compileOnly("org.springframework.boot:spring-boot-starter-data-redis")
 }
