cfb80b046329dc02656b51e0f27ef10934ee785a
silberbullet <pkw961028@naver.com>
2025-05-31 18:19:52 +0900

    build(redis): add test library

--- core/redis/nettee-redis-api/build.gradle.kts
+++ core/redis/nettee-redis-api/build.gradle.kts
@@ -1,3 +1,8 @@
 dependencies{
     api("org.springframework.boot:spring-boot-autoconfigure:3.4.3")
+
+    // test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("com.fasterxml.jackson.core:jackson-databind")
+    testImplementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 }
