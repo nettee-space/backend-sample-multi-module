b2f3f4ba1865a33e86910f6d0cd2a0a48676e3e4
silberbullet <pkw961028@naver.com>
2025-06-03 15:19:24 +0900

    build(redis): add spring data redis library in api module

--- core/redis/nettee-redis-api/build.gradle.kts
+++ core/redis/nettee-redis-api/build.gradle.kts
@@ -1,6 +1,7 @@
 dependencies{
     api("org.springframework.boot:spring-boot-autoconfigure")
-
+    api("org.springframework.boot:spring-boot-starter-data-redis")
+    compileOnly("org.springframework:spring-context")
     // test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("com.fasterxml.jackson.core:jackson-databind")
